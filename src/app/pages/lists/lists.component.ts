dd6070bfb5601237724a1fbd873d201d7b511131
Gaurav <gkgarry911@gmail.com>
2020-05-06 01:39:59 +0530

    add some text and improve fn

--- src/app/pages/lists/lists.component.ts
+++ src/app/pages/lists/lists.component.ts
@@ -1,69 +1,77 @@
-import { IList } from './../../models/list';
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
-import { ApiCallsService } from 'src/app/service/api-calls.service';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { ITask } from 'src/app/models/task';
-import { AuthService } from 'src/app/service/auth.service';
+import { IList } from "./../../models/list";
+import { Component, OnInit } from "@angular/core";
+import { ApiCallsService } from "src/app/service/api-calls.service";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { ITask } from "src/app/models/task";
+import { AuthService } from "src/app/service/auth.service";
 
-var loadFirst  = true ;
+var loadFirst = true;
 @Component({
-  selector: 'app-lists',
-  templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.scss']
+  selector: "app-lists",
+  templateUrl: "./lists.component.html",
+  styleUrls: ["./lists.component.scss"],
 })
 export class ListsComponent implements OnInit {
-
   constructor(
-       private _api : ApiCallsService, 
-       private _activatedroute : ActivatedRoute, 
-       private _route : Router,
-       private _auth : AuthService
-       ) { }
-  lists : IList[] = [];
-  tasks : ITask[] = null;
-  selected_listid : string = "xyz";
-  initLoad : boolean;
+    private _api: ApiCallsService,
+    private _activatedroute: ActivatedRoute,
+    private _route: Router,
+    private _auth: AuthService
+  ) {}
+  lists: IList[] = null;
+  tasks: ITask[] = null;
+  selected_listid: string = "xyz";
+  initLoad: boolean;
   ngOnInit(): void {
-     this._auth.intitalLoad.subscribe(val=>{
-          this.initLoad = val
-     }) 
-     this._api.getlists().subscribe( (result : IList[] ) =>{
-          this.lists = result;
-          if( this.initLoad && this.lists.length> 0){
-              this._route.navigate(['/lists' , this.lists[0]._id])
-              this._auth.intitalLoad.next(false)
-          }
-        })
-     this._activatedroute.paramMap.subscribe((params : ParamMap)=>{     
-          this.selected_listid = params.get('listid')
-          this._api.getTask(this.selected_listid).subscribe( ( tasks : ITask[] )=>{
-               this.tasks = tasks;
-          })
-     }
-     )
-}
-  deleteSingleList(){ 
-    this._api.deleteSingleList(this.selected_listid).subscribe( response  =>{
-          this._route.navigate(['/lists' ])
-    },error=>{ console.log(error)})
+    this._auth.intitalLoad.subscribe((val) => {
+      this.initLoad = val;
+    });
+    this._api.getlists().subscribe((result: IList[]) => {
+      this.lists = result;
+      if (this.initLoad && this.lists.length > 0) {
+        this._route.navigate(["/lists", this.lists[0]._id]);
+        this._auth.intitalLoad.next(false);
+      }
+    });
+    this._activatedroute.paramMap.subscribe((params: ParamMap) => {
+      this.selected_listid = params.get("listid");
+      this._api.getTask(this.selected_listid).subscribe((tasks: ITask[]) => {
+        this.tasks = tasks;
+      });
+    });
   }
-  deleteSingleTask(taskid){
-    this._api.deleteSingleTask(taskid).subscribe(response =>{
-      this.tasks = this.tasks.filter(singletask =>  singletask._id != taskid)
-    }, error => console.log(error) )
+  deleteSingleList() {
+    this._api.deleteSingleList(this.selected_listid).subscribe(
+      (response) => {
+        this._route.navigate(["/lists"]);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-  taskCompleteOps( task : ITask){
-    task.complete = !task.complete
+  deleteSingleTask(taskid) {
+    this._api.deleteSingleTask(taskid).subscribe(
+      (response) => {
+        this.tasks = this.tasks.filter(
+          (singletask) => singletask._id != taskid
+        );
+      },
+      (error) => console.log(error)
+    );
+  }
+  taskCompleteOps(task: ITask) {
+    task.complete = !task.complete;
     let completeObj = {
-      complete : task.complete
-    }
-    this._api.updateSingleTask(this.selected_listid, task._id, completeObj).subscribe((response : ITask)=>{
-    })
+      complete: task.complete,
+    };
+    this._api
+      .updateSingleTask(this.selected_listid, task._id, completeObj)
+      .subscribe((response: ITask) => {});
   }
-  logout(){
-       this._auth.userLogout()
-       this._route.navigate(['/login'])
-       this._auth.intitalLoad.next(true)
+  logout() {
+    this._auth.userLogout();
+    this._route.navigate(["/login"]);
+    this._auth.intitalLoad.next(true);
   }
-
 }
